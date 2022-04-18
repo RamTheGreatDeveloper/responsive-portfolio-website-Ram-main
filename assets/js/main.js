dc4b3444af516f5b58e689dcc7fcdc0104c130f8
Ram kumar <ramkumars.trichy@gmail.com>
2022-04-19 03:07:49 +0530

    script-qualification-section

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,6 +54,29 @@ skillHeader.forEach((el) => {
 
 /*==================== QUALIFICATION TABS ====================*/
 
+const tabs = document.querySelectorAll("[data-target"),
+  tabContent = document.querySelectorAll("[data-content");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+    console.log(target);
+    console.log(tabContent);
+
+    tabContent.forEach((tabContent) => {
+      tabContent.classList.remove("qualification_active");
+    });
+    target.classList.add("qualification_active");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification_active");
+    });
+    console.log(tabs);
+    console.log(tab);
+    tab.classList.add("qualification_active");
+  });
+});
+
 /*==================== SERVICES MODAL ====================*/
 
 /*==================== PORTFOLIO SWIPER  ====================*/
