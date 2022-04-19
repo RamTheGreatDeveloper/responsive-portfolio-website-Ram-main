2a6b92d06732cfb7a54dd890f6043385857678b0
Ram kumar <ramkumars.trichy@gmail.com>
2022-04-19 05:55:03 +0530

    scroll-up

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,5 +121,11 @@ function scrollHeader() {
 }
 window.addEventListener("scroll", scrollHeader);
 /*==================== SHOW SCROLL UP ====================*/
-
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll_up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add("show_scroll");
+  else scrollUp.classList.remove("show_scroll");
+}
+window.addEventListener("scroll", scrollUp);
 /*==================== DARK LIGHT THEME ====================*/
